89d6d5f6714eb20ee50187166b10e286ec632e88
thebishnu <bishnusingh2278@gmail.com>
2026-02-22 12:54:55 +0545

    chore: rename the module and its corresponding changes

---
+++ 05-modules/nginx-web/outputs.tf
@@ -0,0 +1,14 @@
+output "image_id" {
+  description = "ID of the Docker image"
+  value       = module.nginx-web-module.image_id
+}
+
+output "container_id" {
+  description = "ID of the Docker container"
+  value       = module.nginx-web-module.container_id
+}
+
+output "nginx_url" {
+  description = "The URL to access the app"
+  value       = module.nginx-web-module.nginx_url
+}
